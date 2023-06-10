7c7acfb08093c39e0c3ede7c43e37af579605ceb
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-06-10 22:49:55 +0700

    delete preprocessing folder because it's not needed

--- app/app.py
+++ app/app.py
@@ -1,21 +1,13 @@
-# app/app.py
-
-# Main entry point of the Flask application
-# This file contains the code to create and run the Flask app
-# Modify or add routes, middleware, and other necessary setup code here
-
-
 from flask import Flask
-from config import config
 
-def create_app(env='development'):
-    app = Flask(__name__)
-    app.config.from_object(config[env])
-    
-    # Additional app setup and routes...
-    
-    return app
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
-    app = create_app()
+    
     app.run()
---
+++ app/models/knowledge_constraint/__init__.py
@@ -0,0 +1,6 @@
+# app\models\knowledge_constraint\prediction.py
+
+# Functions for model prediction
+# This file contains functions to make predictions using trained ML models
+# Implement prediction functions that take input data and return model predictions
+# Customize these functions based on the specific ML models and prediction requirements of your project
