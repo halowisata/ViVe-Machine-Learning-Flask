c56b3dc588c649f55e8724213e3bde6df014c7e9
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-06-10 22:53:47 +0700

    delete unused files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,6 @@
 # This file is empty or may contain initialization code specific to the models package
 # It is used to mark the 'models' folder as a Python package
 # Modify or add code here to initialize the models package if needed
+
+import pandas as pd
+import numpy as np
--- app/models/knowledge_constraint/__init__.py
+++
@@ -1,6 +0,0 @@
-# app\models\knowledge_constraint\prediction.py
-
-# Functions for model prediction
-# This file contains functions to make predictions using trained ML models
-# Implement prediction functions that take input data and return model predictions
-# Customize these functions based on the specific ML models and prediction requirements of your project
